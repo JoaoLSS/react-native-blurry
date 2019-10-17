8a35bb5529606391806dc3164ad9bc6f80a54691
João Lucas Silveira Silva <joao.lucas.ss@hotmail.com>
2019-10-17 17:47:37 -0300

    some fixes

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import { requireNativeComponent, ViewProps } from 'react-native';
 
-export default requireNativeComponent("RCTBlurView") as React.Component<ViewProps & { radius: number, sampling: number }>
+export default requireNativeComponent("RCTBlurView") as React.JSXElementConstructor<ViewProps & { radius: number, sampling: number }>
