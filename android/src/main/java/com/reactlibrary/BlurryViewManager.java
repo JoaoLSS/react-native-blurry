a3e091ff50bd00bf8874d09ca3e96f6b9f1b4f0d
João Lucas Silveira Silva <joao.lucas.ss@hotmail.com>
2019-10-21 11:05:27 -0300

    minor changes

--- android/src/main/java/com/reactlibrary/BlurryViewManager.java
+++ android/src/main/java/com/reactlibrary/BlurryViewManager.java
@@ -53,7 +53,7 @@ public class BlurryViewManager extends SimpleViewManager<ReactImageView> {
     private void setBlurred(ReactImageView view) {
         try {
             if(bitmap==null) {
-                View focusedView = BlurryModule.mModule.getActivity().getWindow().getDecorView().findViewById(android.R.id.content);
+                View focusedView = BlurryModule.mModule.getActivity().getWindow().getDecorView().getRootView();
                 if(focusedView!=null) {
                     Log.d("RNBLURRY", "taking screenshot");
                     focusedView.setDrawingCacheEnabled(true);
