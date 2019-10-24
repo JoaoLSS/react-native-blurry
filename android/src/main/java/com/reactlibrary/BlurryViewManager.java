b097e495837c0c2543f63e6e9e8c83c02b6b72fc
João Lucas Silveira Silva <joao.lucas.ss@hotmail.com>
2019-10-24 11:07:46 -0300

    minor changes

--- android/src/main/java/com/reactlibrary/BlurryViewManager.java
+++ android/src/main/java/com/reactlibrary/BlurryViewManager.java
@@ -6,12 +6,15 @@ import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.PixelCopy;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowInsets;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.react.bridge.Arguments;
@@ -34,7 +37,6 @@ public class BlurryViewManager extends SimpleViewManager<ReactImageView> {
     private int mRadius = 20;
     private int mSampling = 1;
     private boolean mVisible = false;
-    private String mColor = "#00000000";
     private Bitmap bitmap;
 
     BlurryViewManager(ReactApplicationContext reactContext) {
@@ -53,75 +55,52 @@ public class BlurryViewManager extends SimpleViewManager<ReactImageView> {
         return new ReactImageView(reactContext, Fresco.newDraweeControllerBuilder(), null, mContext);
     }
 
-    private void setBlurred(ReactImageView view) {
-        try {
-            Blurry.with(mContext)
-                    .color(Color.parseColor(mColor))
-                    .radius(mRadius)
-                    .sampling(mSampling)
-                    .animate()
-                    .from(bitmap)
-                    .into(view);
-        }
-        catch(Exception e) {
-            Log.d("RNBLURRY", e.getMessage());
-        }
-    }
-
-    @ReactProp(name="radius")
-    public void setRadius(ReactImageView view, int radius) {
-        this.mRadius = radius;
-        if(mVisible) setBlurred(view);
-    }
+    @ReactProp(name = "radius")
+    public void setRadius(ReactImageView view, int radius) { this.mRadius = radius; }
 
     @ReactProp(name="sampling")
-    public void setSampling(ReactImageView view, int sampling) {
-        this.mSampling = sampling;
-        if(mVisible) setBlurred(view);
-    }
-
-    @ReactProp(name="overlayColor")
-    public void setColor(ReactImageView view, String color) {
-        mColor = color;
-        if(mVisible) setBlurred(view);
-    }
-
-    @ReactProp(name="alpha")
-    public void setAlpha(ReactImageView view, int alpha) {
-        view.setImageAlpha(alpha);
-    }
+    public void setSampling(ReactImageView view, int sampling) { this.mSampling = sampling; }
 
     @ReactProp(name="visible")
-    public void setVisible(final ReactImageView view, boolean visible) {
+    public void setVisible(ReactImageView view, boolean visible) {
         if(visible && !mVisible) {
-            Log.d("RNBLURRY", "overlay is visible, getting screenshot");
             final Rect rect = new Rect();
             Window window = BlurryModule.mModule.getActivity().getWindow();
             window.getDecorView().getWindowVisibleDisplayFrame(rect);
-            int navBarHeight = window.getDecorView().getRootWindowInsets().getStableInsetBottom();
-            final int statusBarHeight = window.getDecorView().getRootWindowInsets().getStableInsetTop();
-            final Bitmap unscaledBitmap = Bitmap.createBitmap(rect.width(), rect.height() + navBarHeight + statusBarHeight, Bitmap.Config.ARGB_8888);
-            PixelCopy.request(window, unscaledBitmap, new PixelCopy.OnPixelCopyFinishedListener() {
-                @Override
-                public void onPixelCopyFinished(int i) {
-                    if(i==PixelCopy.SUCCESS) {
-                        unscaledBitmap.reconfigure(rect.width(), rect.height() + statusBarHeight, Bitmap.Config.ARGB_8888);
-                        bitmap = Bitmap.createBitmap(unscaledBitmap);
-                        view.setBackground(new BitmapDrawable(mContext.getResources(), bitmap));
-                        setBlurred(view);
-                        mVisible = true;
-                        mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("RNBLURRY", true);
+            WindowInsets insets = window.getDecorView().getRootWindowInsets();
+            final int height = rect.height() + insets.getStableInsetTop();
+            int navBarHeight = insets.getStableInsetBottom();
+            final Bitmap unscaledBitmap = Bitmap.createBitmap(rect.width(), height+navBarHeight, Bitmap.Config.ARGB_8888 );
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                PixelCopy.request(window, unscaledBitmap, new PixelCopy.OnPixelCopyFinishedListener() {
+                    @Override
+                    public void onPixelCopyFinished(int i) {
+                        if(i==PixelCopy.SUCCESS) {
+                            unscaledBitmap.reconfigure(rect.width(), height, Bitmap.Config.ARGB_8888);
+                            bitmap = Bitmap.createBitmap(unscaledBitmap);
+                            mVisible = true;
+                            mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("RNBLURRY", true);
+                        }
                     }
-                }
-            }, new Handler(new Handler.Callback() {
-                @Override
-                public boolean handleMessage(Message message) {
-                    Log.d("RNBLURRY", message.toString());
-                    return false;
-                }
-            }));
+                }, new Handler(new Handler.Callback() {
+                    @Override
+                    public boolean handleMessage(Message message) {
+                        Log.d("RNBLURRY", message.toString());
+                        return false;
+                    }
+                }));
+            }
+            else {
+                View _view = window.getDecorView();
+                _view.setDrawingCacheEnabled(true);
+                _view.destroyDrawingCache();
+                _view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
+                bitmap = _view.getDrawingCache();
+                mVisible = true;
+                mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("RNBLURRY", true);
+            }
         }
-        else if(!visible) {
+        if(!visible) {
             mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("RNBLURRY", false);
             if(bitmap != null) bitmap.recycle();
             view.setImageDrawable(null);
@@ -130,11 +109,104 @@ public class BlurryViewManager extends SimpleViewManager<ReactImageView> {
         }
     }
 
-    @ReactProp(name="source")
-    public void setSource(ReactImageView view, String uri) {
-        Uri imageUri = Uri.parse(uri);
-        try { bitmap = MediaStore.Images.Media.getBitmap(BlurryModule.mModule.getActivity().getContentResolver(), imageUri); }
-        catch (Exception ignored) {}
+    @ReactProp(name="viewType")
+    public void setView(ReactImageView view, String viewType) {
+        if(bitmap!=null && viewType!=null) {
+            if(viewType.equals("background")) {
+                view.setBackground(new BitmapDrawable(mContext.getResources(), bitmap));
+            }
+            else {
+                Blurry.with(mContext)
+                        .radius(mRadius)
+                        .sampling(mSampling)
+                        .from(bitmap)
+                        .into(view);
+            }
+        }
     }
+
+//    private void setBlurred(ReactImageView view) {
+//        try {
+//            Blurry.with(mContext)
+//                    .color(Color.parseColor(mColor))
+//                    .radius(mRadius)
+//                    .sampling(mSampling)
+//                    .animate()
+//                    .from(bitmap)
+//                    .into(view);
+//        }
+//        catch(Exception e) {
+//            Log.d("RNBLURRY", e.getMessage());
+//        }
+//    }
+//
+//    @ReactProp(name="radius")
+//    public void setRadius(ReactImageView view, int radius) {
+//        this.mRadius = radius;
+//        if(mVisible) setBlurred(view);
+//    }
+//
+//    @ReactProp(name="sampling")
+//    public void setSampling(ReactImageView view, int sampling) {
+//        this.mSampling = sampling;
+//        if(mVisible) setBlurred(view);
+//    }
+//
+//    @ReactProp(name="overlayColor")
+//    public void setColor(ReactImageView view, String color) {
+//        mColor = color;
+//        if(mVisible) setBlurred(view);
+//    }
+//
+//    @ReactProp(name="alpha")
+//    public void setAlpha(ReactImageView view, int alpha) {
+//        view.setImageAlpha(alpha);
+//    }
+//
+//    @ReactProp(name="visible")
+//    public void setVisible(final ReactImageView view, boolean visible) {
+//        if(visible && !mVisible) {
+//            Log.d("RNBLURRY", "overlay is visible, getting screenshot");
+//            final Rect rect = new Rect();
+//            Window window = BlurryModule.mModule.getActivity().getWindow();
+//            window.getDecorView().getWindowVisibleDisplayFrame(rect);
+//            int navBarHeight = window.getDecorView().getRootWindowInsets().getStableInsetBottom();
+//            final int statusBarHeight = window.getDecorView().getRootWindowInsets().getStableInsetTop();
+//            final Bitmap unscaledBitmap = Bitmap.createBitmap(rect.width(), rect.height() + navBarHeight + statusBarHeight, Bitmap.Config.ARGB_8888);
+//            PixelCopy.request(window, unscaledBitmap, new PixelCopy.OnPixelCopyFinishedListener() {
+//                @Override
+//                public void onPixelCopyFinished(int i) {
+//                    if(i==PixelCopy.SUCCESS) {
+//                        unscaledBitmap.reconfigure(rect.width(), rect.height() + statusBarHeight, Bitmap.Config.ARGB_8888);
+//                        bitmap = Bitmap.createBitmap(unscaledBitmap);
+//                        view.setBackground(new BitmapDrawable(mContext.getResources(), bitmap));
+//                        setBlurred(view);
+//                        mVisible = true;
+//                        mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("RNBLURRY", true);
+//                    }
+//                }
+//            }, new Handler(new Handler.Callback() {
+//                @Override
+//                public boolean handleMessage(Message message) {
+//                    Log.d("RNBLURRY", message.toString());
+//                    return false;
+//                }
+//            }));
+//        }
+//        else if(!visible) {
+//            mContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("RNBLURRY", false);
+//            if(bitmap != null) bitmap.recycle();
+//            view.setImageDrawable(null);
+//            view.setBackground(null);
+//            this.mVisible = false;
+//        }
+//    }
+//
+//    @ReactProp(name="source")
+//    public void setSource(ReactImageView view, String uri) {
+//        Uri imageUri = Uri.parse(uri);
+//        try { bitmap = MediaStore.Images.Media.getBitmap(BlurryModule.mModule.getActivity().getContentResolver(), imageUri); }
+//        catch (Exception ignored) {}
+//    }
 
 }
--- index.tsx
+++ index.tsx
@@ -1,47 +1,63 @@
 import React, { useEffect, useState } from "react"
-import { requireNativeComponent, ViewProps, Dimensions, View, NativeAppEventEmitter, StatusBar } from 'react-native';
-import { captureScreen } from "react-native-view-shot"
+import { requireNativeComponent, ViewProps, Dimensions, NativeAppEventEmitter } from 'react-native'
+import Reanimated from "react-native-reanimated"
+
 
 const RCTBlurView = requireNativeComponent("RCTBlurView") as React.JSXElementConstructor<ViewProps & {
     radius: number,
     sampling: number,
     visible: boolean,
-    alpha ?: number,
-    source ?: string,
-    overlayColor ?: string
+    viewType: "background"|"blur"|null,
+    children ?: React.ReactNode
 }>
 
 export const BlurOverlay = (props: {
     radius: number
     sampling: number
     visible: boolean
-    source ?: string
-    overlayColor ?: string
     children ?: React.ReactNode
-    alpha ?: number
+    animate: Reanimated.Node<any>
 }) => {
 
     const [reallyVisible, setReallyVisible] = useState(false)
 
     const { width, height } = Dimensions.get("window")
 
-    useEffect(() => NativeAppEventEmitter.addListener("RNBLURRY", (visible) => {
-        console.log({ visible })
-        setReallyVisible(visible)
-    }).remove, [])
+    useEffect(() => NativeAppEventEmitter.addListener("RNBLURRY", setReallyVisible).remove, [])
 
     return (
-        <View style={{ backgroundColor: "transparent", position: "absolute", top: 0, left: 0 }}>
-            <RCTBlurView
-                style={{ width, height }}
-                radius={props.radius}
-                sampling={props.sampling}
-                visible={props.visible && !props.source}
-                source={props.source}
-                overlayColor={props.overlayColor}
-                alpha={props.alpha}
-            />
-        </View>
+        <RCTBlurView
+            style={{ backgroundColor: "transparent", position: "absolute", top: 0, left: 0, width, height }}
+            radius={props.radius}
+            sampling={props.sampling}
+            visible={props.visible}
+            viewType={reallyVisible ? "background" : null}
+        >
+            <Reanimated.View
+                style={{ opacity: props.animate }}
+            >
+                <RCTBlurView
+                    style={{ backgroundColor: "transparent", position: "absolute", top: 0, left: 0, width, height }}
+                    radius={props.radius}
+                    sampling={props.sampling}
+                    visible={props.visible}
+                    viewType={reallyVisible ? "blur" : null}
+                >
+                    {props.children}
+                </RCTBlurView>
+            </Reanimated.View>
+        </RCTBlurView>
+        // <View style={{ backgroundColor: "transparent", position: "absolute", top: 0, left: 0 }}>
+        //     <RCTBlurView
+        //         style={{ width, height }}
+        //         radius={props.radius}
+        //         sampling={props.sampling}
+        //         visible={props.visible && !props.source}
+        //         source={props.source}
+        //         overlayColor={props.overlayColor}
+        //         alpha={props.alpha}
+        //     />
+        // </View>
     )
 
 }
